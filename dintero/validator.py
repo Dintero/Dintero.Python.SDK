c2c14f1a8d1dae8b03db1ab081027fde63109828
Anders Emil Salvesen <andersems@gmail.com>
2021-06-21 21:12:46 +0200

    formatted

--- dintero/validator.py
+++ dintero/validator.py
@@ -2,5 +2,9 @@ from dintero.error import InvalidFieldError
 
 
 def validate_session(session):
-    if session['order']['amount'] != sum([item['amount'] for item in session['order']['items']]):
-        raise InvalidFieldError('order.amount doesn\'t match sum of order.items', 'order.amount')
+    if session["order"]["amount"] != sum(
+        [item["amount"] for item in session["order"]["items"]]
+    ):
+        raise InvalidFieldError(
+            "order.amount doesn't match sum of order.items", "order.amount"
+        )
